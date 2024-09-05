0fa4f140ca1244fa0ec74491b8284ccfcad67ad6
Nur Hazirah Md Yusof <nurhazirah229@gmail.com>
2024-09-05 11:51:08 +0800

    Update streamlit_app.py
    add json format

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,4 +58,4 @@ if ingredients_list:
 # new section to display fruitvice nutrition info
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
